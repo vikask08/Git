c6d81bf5ee5cd97c4fd6931ae78cd460016f48c5
vikask08 <zvikas.8294@gmail.com>
2022-01-28 22:36:52 +0530

    TestCase4 added to new branch

--- src/test/java/GitDemo/TestCase03.java
+++ src/test/java/GitDemo/TestCase03.java
@@ -3,7 +3,7 @@ package GitDemo;
 public class TestCase03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Git");
 
 	}
 
---
+++ src/test/java/branch/TestCase4.java
@@ -0,0 +1,10 @@
+package branch;
+
+public class TestCase4 {
+
+	public static void main(String[] args) {
+		System.out.println("keep this testcase 4 in new branch");
+
+	}
+
+}
