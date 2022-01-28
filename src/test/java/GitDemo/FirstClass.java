be12d67a3607fcbc79e58ea67ca066b71931a21e
vikask08 <zvikas.8294@gmail.com>
2022-01-28 22:03:21 +0530

    TestCase 2 automated

--- src/test/java/GitDemo/FirstClass.java
+++ src/test/java/GitDemo/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("share this project on github");
-
+         System.out.println("adding one more extra line");
 	}
 
 }
---
+++ src/test/java/GitDemo/TestCase002.java
@@ -0,0 +1,10 @@
+package GitDemo;
+
+public class TestCase002 {
+
+	public static void main(String[] args) {
+		System.out.println("Testcase 2 for Automation ");
+
+	}
+
+}
