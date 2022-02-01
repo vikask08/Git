f24528bf24d95c9a1e7346232b83162d79a5d50c
vikask08 <zvikas.8294@gmail.com>
2022-02-01 11:16:09 +0530

    Added GenericUtility class and TC4

--- src/test/java/branch/TestCase4.java
+++ src/test/java/branch/TestCase4.java
@@ -4,7 +4,7 @@ public class TestCase4 {
 
 	public static void main(String[] args) {
 		System.out.println("keep this testcase 4 in new branch");
-
+        System.out.println("added new line in Test Case 4");
 	}
 
 }
